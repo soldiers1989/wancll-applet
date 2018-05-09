1a3774dd7bd9e34b21a8f788036471e06b1ef9aa
GJ Wang <itellboy@foxmail.com>
2018-05-09 22:46:27 +0800

    regular

--- pages/login/login.js
+++ pages/login/login.js
@@ -8,8 +8,49 @@ Page({
   onLoad: function (options) {
 
   },
+  // 手机号码监听
+  bindMobileInput(e) {
+    this.setData({
+      mobile: e.detail.value
+    })
+  },
+  // 密码监听
+  bindPasswordInput(e) {
+    this.setData({
+      password: e.detail.value
+    })
+  },
+  // 登陆监听
   login() {
-    console.log(this.data.mobile);
-    console.log(this.data.password);
+    if (!this.data.mobile) {
+      wx.showToast({
+        title: '请填写手机号码',
+        icon: 'none',
+      })
+      return;
+    }
+    if (!this.data.password) {
+      wx.showToast({
+        title: '请填写密码',
+        icon: 'none',
+      })
+      return;
+    }
+    app.httpRequest('api_users/user_accounts/login', {
+      mobile: this.data.mobile,
+      password: this.data.password,
+    }, (resp) => {
+      console.log(resp)
+    });
+  },
+  // 微信登陆监听
+  wechatLogin() {
+    wx.login({
+      success(res) {
+        if (res.code) {
+          console.log(res.code)
+        }
+      }
+    })
   }
 })
