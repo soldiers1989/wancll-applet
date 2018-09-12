24e9faeaf8d089a3f5c9d495885bc69d7d5b36f8
KBellX <1017990427@qq.com>
2018-09-12 16:18:03 +0800

    会员权益图片过大bug解决

--- pages/ComMemberArticle/index.js
+++ pages/ComMemberArticle/index.js
@@ -7,12 +7,14 @@ Page({
     APP.ajax({
       url: APP.api.articleArticle,
       data: {
-        id:options.id
+        id: options.id
       },
       success: res => {
         wx.setNavigationBarTitle({
           title: res.data.title,
         })
+        
+        res.data.content = res.data.content.replace('<img', '<img style="max-width:100%;height:auto" ');
         this.setData({
           article: res.data
         })
