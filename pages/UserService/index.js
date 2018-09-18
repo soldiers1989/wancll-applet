d7f83a556c5e6a6768ba985c019ba0dc61a0dd8d
KBellX <1017990427@qq.com>
2018-09-18 13:33:36 +0800

    mod

--- pages/UserService/index.js
+++ pages/UserService/index.js
@@ -1,27 +1,37 @@
 const APP = getApp();
+import PagingData from '../../utils/PagingData';
+const Paging = new PagingData();
 Page({
   data: {
+    FPage: {
+      pageNum: 1,
+      hasData: true,
+      noContent: false,
+      noContentImg: APP.imgs.noContentImg
+    },
     orderList: []
   },
   onLoad(options) {
-    let that = this;
-    APP.ajax({
-      url: APP.api.orderAll,
-      data: { is_has_return_goods: 1 },
-      success(res) {
-        console.log(res.data)
-        that.setData({ orderList: res.data })
-        wx.setStorageSync('orderList', res.data)
+    Paging.init({
+      type: 2,
+      that: this,
+      url: 'orderAll',
+      pushData: 'orderList',
+      getFunc: this.getOrderData
+    })
+    this.getOrderData();
+  },
+  getOrderData() {
+    Paging.getPagesData({
+      postData: {
+        is_has_return_goods: 1
       }
     })
   },
   onPullDownRefresh() {
-
+    Paging.refresh()
   },
-  onReachBottom() {
-
-  },
-  onShareAppMessage() {
-
+  onReachBottom: function() {
+    this.getOrderData()
   }
 })
