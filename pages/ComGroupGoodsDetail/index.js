f1c11fea1af3b5e905b9cc021aae70043540c49f
KBellX <1017990427@qq.com>
2018-09-27 09:40:05 +0800

    修改商品详情页

--- pages/ComGroupGoodsDetail/index.js
+++ pages/ComGroupGoodsDetail/index.js
@@ -229,7 +229,7 @@ Page({
       path += '&parent_mobile=' + this.data.user.mobile;
     }
     return {
-      title: this.data.goods.goods_info.name + '（我正在拼这件好货，人多力量大。是朋友就来帮我一起砍价！）',
+      title: '（我正在拼这件好货，人多力量大。是朋友就来帮我一起砍价！）' + this.data.goods.goods_info.name,
       path: path
     }
   }
