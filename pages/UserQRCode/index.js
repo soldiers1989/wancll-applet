4098cbb827ac4133aa7e0768d4e2258c555d600c
GJ Wang <itellboy@foxmail.com>
2018-11-05 09:39:55 +0800

    fix

--- pages/UserQRCode/index.js
+++ pages/UserQRCode/index.js
@@ -5,23 +5,25 @@ var config = require("../../api/config.js")
 Page({
   data: {
     share_img: '',
+    qrcodeImg: ''
   },
   canvasId: "canvas",
   onLoad(options) {
     // 组装url
-    let host = config.defaultHost
-    let str = '/wap/index/handle_qrcode.html?parent_mobile='
-    let userMobile = wx.getStorageSync("user").mobile
-    let url = host + str + userMobile
-    new QRCode('canvas', {
-      text: url,
-      width: 150,
-      height: 150,
-      colorDark: "#000000",
-      colorLight: "#ffffff",
-    })
+    // let host = config.defaultHost
+    // let str = '/wap/index/handle_qrcode.html?parent_mobile='
+    // let userMobile = wx.getStorageSync("user").mobile
+    // let url = host + str + userMobile
+    // new QRCode('canvas', {
+    //   text: url,
+    //   width: 150,
+    //   height: 150,
+    //   colorDark: "#000000",
+    //   colorLight: "#ffffff",
+    // })
   },
   onShow() {
+    let user = wx.getStorageSync('user')
     // 加载背景图
     APP.ajax({
       url: APP.api.systemParams,
@@ -33,14 +35,23 @@ Page({
         share_img: res.data.share_img || APP.imgs.myEwmCode
       })
     }).catch(err => {})
+    wx.showLoading({
+      title: '加载中',
+    })
     APP.ajax({
       url: APP.api.shareQrcode,
       data: {
-        path: 'pages/ComRegister/index',
+        path: 'pages/ComRegister/index?parent_mobile=' + user.mobile,
+        // path: 'pages/ComRegister/index',
       }
     }).then(res => {
-
-    }).catch(err => {})
+      wx.hideLoading()
+      this.setData({
+        qrcodeImg: res.data
+      })
+    }).catch(err => {
+      wx.hideLoading()
+    })
   },
   onShareAppMessage() {
     return {
